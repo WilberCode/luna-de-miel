da8892336a17ef744ef87234ab26ff6c5eb090e1
Wilber <parionahuaraca@gmail.com>
2019-11-26 12:01:07 -0500

    csmbios

--- 404.php
+++ 404.php
@@ -9,8 +9,11 @@
   <div class="container">
 	<div class="col-main col-sm-12">
 		<h2>Upss... No encontramos la información solicitada.</h2>
-		<p>Te invitamos a navegues por nuestra web para encontrar la información requerida:</p>
+		<p>Te invitamos a que navegues por nuestra web para encontrar la información requerida:</p>
 		<?php wp_nav_menu( array('theme_location' => 'sidebar','menu_class' => 'menu-sidebar') ); ?>
+		<div class="col-xs-12 col-md-12 text-center">
+				<a class="btn btn-default btn-lg" href="<?php echo esc_url( home_url( '/' ) ); ?>" role="button">Quiero salir de aqui</a>
+			</div>	
 	</div>
   </div>
 </section>
