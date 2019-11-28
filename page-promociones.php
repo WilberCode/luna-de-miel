0b0a61b59c5de0d7cbfd007d13187a3204c6c9f5
Wilber <parionahuaraca@gmail.com>
2019-11-28 15:01:09 -0500

    se agrego cambios

--- page-promociones.php
+++ page-promociones.php
@@ -84,7 +84,7 @@
 								 $imagethumb = get_template_directory_uri()."/_/images/thumb-default.jpg";
 							endif;
 							echo '<div class="col-xs-12 col-sm-6 col-md-3 post-card">';
-							echo '<a   href="' . get_permalink() . '"  ><img class="thumbnail"  src=" ' . $imagethumb . '" alt="'.get_the_title().'"/>';
+							echo '<a   href="' . get_permalink() . '" ><img class="thumbnail"  src=" ' . $imagethumb . '" alt="'.get_the_title().'">';
 							echo '<h2 class="title-list">'.get_the_title().'</h2>';
 							echo '</a>'; 
 							echo '</div>';
