4ead2b509117e0ce6b08efc23b20d832d663ebc7
Wilber <parionahuaraca@gmail.com>
2020-02-06 12:23:04 -0500

    se agrego cambios

--- page.php
+++ page.php
@@ -5,21 +5,7 @@
  * @since HTML5 Reset 2.0
  * 
  */
- get_header(); ?>
-	<section class="publicidad hidden-xs">
-		<div class="container">
-			<div class="row">
-				<div class="banner-top-728x90">			
-					<!-- /22596825/Home_Top_Skycrapper -->
-					<div id='div-gpt-ad-1459740034135-4' style='height:90px; width:728px;'>
-					<script type='text/javascript'>
-					googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-4'); });
-					</script>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+ get_header(); ?> 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<section id="main" class="clearfix">
 		<div class="container page-wrap">
