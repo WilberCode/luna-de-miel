2447f8ac77b9cdecf780774b8fd9c18ebc28416a
Wilber <parionahuaraca@gmail.com>
2020-02-10 13:16:42 -0500

    Se agrego cambios

--- taxonomy-rubros.php
+++ taxonomy-rubros.php
@@ -4,25 +4,11 @@
  * @subpackage HTML5-Reset-WordPress-Theme
  * @since HTML5 Reset 2.0
  */
- get_header(); ?>
-<section class="publicidad hidden-xs">
-	<div class="container">
-		<div class="row">
-			<div class="banner-top-728x90">			
-				<!-- /22596825/Home_Top_Skycrapper -->
-				<div id='div-gpt-ad-1459740034135-4' style='height:90px; width:728px;'>
-				<script type='text/javascript'>
-				googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-4'); });
-				</script>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<section id="main" class="clearfix">
+ get_header(); ?> 
+<section id="main" class="clearfix provider-single" >
 	<div class="container">
 		<div class="col-main col-sm-12">
-			<h2><?php echo single_cat_title(''); ?></h2>
+			<h1><?php echo single_cat_title(''); ?></h1>
 			<div class="entry">
 				<?php echo category_description(); ?>
 			</div>
@@ -129,18 +115,7 @@
 			</div>
 			<?php endif; ?>
 		</div>
-		<div class="col-sm-4 text-center">
-			<!--BRASIL-->
-			<!-- <div class="banner-right-300x250">  -->
-				<!-- /22596825/Home_Right_1_300x250 
-				<div id='div-gpt-ad-1459740034135-2' style='height:250px; width:300px;'>-->
-				<!-- <div style='height:320px; width:300px;'> -->
-			<!-- <a href="http://www.losincas.com.pe/bodaslima" target="_blank"><img src="http://lunademiel.com.pe/emailing/images/Mailing_luna-de-miel-.png" style="height: 320px; width: 300px" /></a> -->
-				<!-- <script type='text/javascript'>
-				//googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-2'); });
-				// </script> -->
-			<!-- </div> -->
-			<!--FIN BRASIL-->
+		<div class="col-sm-4 text-center"> 
 			<?php
 				$terms = get_terms( array('taxonomy' => 'rubros','hide_empty' => false,'orderby'=>'count','order'=>'desc','number'=>10));
 			?>
@@ -150,15 +125,7 @@
 			  <a href="/rubros/<?php echo $term->slug?>/" class="list-group-item"><?php echo $term->name; ?><span class="badge"><?php echo $term->count; ?></span></a>
 			<?php }?>
 			  <a href="/proveedores/" class="list-group-item">Ver más categorías</a>
-			</div>
-			<div class="banner-right-300x250">
-				<!-- /22596825/Home_Right_2_300x250 -->
-				<div id='div-gpt-ad-1459740034135-3' style='height:250px; width:300px;'>
-				<script type='text/javascript'>
-				googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-3'); });
-				</script>
-				</div>
-			</div>
+			</div> 
 		</div>
 	</div>
 </section>
