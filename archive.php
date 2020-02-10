80a978d8f67cce36d7af12cb3334907eab0cb6b8
Wilber <parionahuaraca@gmail.com>
2020-02-10 11:33:33 -0500

    se agrego cambios en la archive

--- archive.php
+++ archive.php
@@ -45,7 +45,7 @@
 			</div> 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
+				<ul class="nav navbar-nav archive-tab">
 					
 					<?php 
 					 if ( get_post_type( get_the_ID() ) == 'promociones') {
