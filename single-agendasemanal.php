0db9fca24df5d14725b1582154bf734b2295e2c7
wilber <parionahuaraca@gmail.com>
2021-05-21 17:52:32 -0500

    cambios

--- single-agendasemanal.php
+++ single-agendasemanal.php
@@ -4,119 +4,7 @@
  * @subpackage HTML5-Reset-WordPress-Theme
  * @since HTML5 Reset 2.0
  */
-/** 
- get_header(); ?>
-<section id="main" class="clearfix">
-  <div class="container">
-	<div class="col-main col-sm-8">
-		<?php if (get_post_status()=='archive'){ ?>
-			<div class="alert alert-warning" role="alert">
-			  Este evento ya no se encuentra disponible. Te invitamos a que visites <a href="/agenda-semanal/" class="alert-link">la agenda semanal</a>.
-			</div>
-		<?php } ?>
-		<section class="head-title">	
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<h1><?php echo get_the_title(); ?></h1>
-			<?php endwhile; endif; ?>
-		</section>
-		<?php //if ( has_post_thumbnail() ):?>
-			<!--<div class="row">
-				<div class="featured-image-wrap text-center"><?php the_post_thumbnail(); ?></div>
-			</div>-->
-		<?php //endif; ?>	
-		<div class="entry">
-			<?php the_content(); ?>
-		</div>
-	<section class="publicidad hidden-xs">
-			<div class="row">
-				<div class="banner-footer-728x90">			
-					<!-- /22596825/Home_Footer_Skycrapper -->
-					<div id='div-gpt-ad-1459740034135-0' style='height:90px; width:728px;'>
-					<script type='text/javascript'>
-					googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-0'); });
-					</script>
-					</div>
-				</div>
-			</div>
-	</section>			
-		<!--<section class="comentarios">
-			<?php //comments_template(); ?>
-		</section>-->
-  	</div>
-	<div class="col-sidebar col-sm-1"></div>
-	<div class="col-sidebar col-sm-3">
-	  	<div class="row show-grid">
-			<h3>Los más recientes</h3>
-			<div class="container-fluid mas-recientes">
-			<?php
-					global $post,$categoria;
-					
-					//query subpages
-					$args = array(
-					'post_type' => 'agendasemanal',
-					'orderby' => 'date',
-					'order' => 'desc',
-					'post__not_in' => array(get_the_ID()),
-					'posts_per_page' => 3
-					);
-
-					$listing = new WP_query($args);
-
-					// create output
-					if ($listing->have_posts()) :
-						while ($listing->have_posts()) : $listing->the_post();
-							$arr_image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'img-sidebar');
-							if ( $arr_image[0] != '' ) :
-								 $imagethumb = $arr_image[0];
-							else :
-								 $imagethumb = get_template_directory_uri()."/_/images/thumb-default.jpg";
-							endif;
-							
-							$terms = wp_get_post_terms( $listing->post->ID, array( 'agenda' ) ); 
-							
-							$categoria = $terms[0]->name;
-							
-							echo '<div class="row">';
-							echo '<div class="col-xs-4 col-md-4 cinco-padding"><a href="' . get_permalink() . '" rel="nofollow" class="thumbnail"><img src="' . $imagethumb . '" alt="'.get_the_title().'"/></a></div>';
-							echo '<div class="col-xs-8 col-md-8 cinco-padding"><p>'.$categoria.'</p><a href="' . get_permalink() . '" class="title-list">'.get_the_title().'</a></div>';
-							echo '</div><hr class="clean"/>';
-						endwhile;
-					endif;
-
-					// reset the query
-					wp_reset_postdata();
-				  ?>
-				 </div>
-	  	</div>
-		<!--BRASIL-->
-		<div class="banner-right-300x250">
-			<!-- /22596825/Home_Right_1_300x250 
-			<div id='div-gpt-ad-1459740034135-2' style='height:250px; width:300px;'>-->
-			<div style='height:300px; width:300px;'>
-			<a href="http://www.lunademiel.com.pe/contacto/" target="_blank"><img src="http://www.lunademiel.com.pe/emailing/logo/b3.png" style="height: 280px; width: 300px" /></a>
-			<script type='text/javascript'>
-			//googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-2'); });
-			</script>
-		</div>
-		<!--FIN BRASIL-->
-		<div>
-			<h3>También visita</h3>
-			<?php wp_nav_menu( array('theme_location' => 'sidebar','menu_class' => 'menu-sidebar') ); ?>
-	  	</div>
-	  	<div class="banner-right-300x250">
-			<!-- /22596825/Home_Right_2_300x250 -->
-			<div id='div-gpt-ad-1459740034135-3' style='height:250px; width:300px;'>
-			<script type='text/javascript'>
-			googletag.cmd.push(function() { googletag.display('div-gpt-ad-1459740034135-3'); });
-			</script>
-			</div>
-		</div>
-	</div> 
-  </div>
-</section>
-<?php get_footer(); ?>
-*/ 
-?>
+ ?>
  <?php  get_header(); 
 $number_og = 1;
 $number_single = 1;
@@ -143,108 +31,46 @@ $number_single = 1;
 <!-- 
   <link rel="stylesheet" href="http://www.lunademiel.com.pe/emailing/2019/11-noviembre/agenda/css/bastemp.min.css">
   <link rel="stylesheet" href="http://www.lunademiel.com.pe/emailing/2019/11-noviembre/agenda/css/styles.min.css"> -->
-
    <!-- Owl -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/ajax-loader.gif">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.green.min.css"> 
-  <style> 
-  h1,h2,h3,h4,h5{
-	font-family: 'Times New Roman', Times, serif;
-    font-weight: 100;
-	margin: .3vw 0;
-	color:black;
-  }
-  h1{
-	  font-size: 5rem;
-	      
-  }
-  h2{
-	   font-size: 3rem !important;
-	   
-  }
-  @media (max-width:400px) {
-	h1{
-	   font-size: 3rem !important; 
-	  } 
-	h1 span{
-		font-size: 4rem !important; 
-	} 
-	h2{
-		font-size: 2rem !important;
-	
-	}
-  }
-  p img{
-	  width:100% !important;  
-  }
-  img{
-      vertical-align: unset !important;
-  }
-  .agenda-single  p img{
-	  width:100% !important;
-	  height:auto;
-  }
-  a{
-	  color: #EB3D82; 
-  }
-  a:hover{
-      color:#eb3d82;
-  }
-  p,div{
-      color:black;
-  }
-  p{
-	  font-size: 20px; 
-  }
-  header{
-      display:none; 
-  }
-  .at-above-post.addthis_tool, .at-below-post.addthis_tool{
-      display:none;
-  }
-  body{
-    font-size: 1.6rem !important;
-    line-height: normal;
-  }
-  
-  </style>
  
 </head>
 <body class=" "> 
 <div class=" ">
  
-  <section class="max-w-[980px] space-x-2 m-auto "> 
-   		<div  class="flex flex-col md:flex-row justify-between" >
-			<div class=" flex justify-between sm:block" >
+  <section class="agenda max-w-[980px]  m-auto "> 
+   		<div  class=" flex flex-col md:flex-row justify-between px-4 lg:px-0" >
+			<div class=" flex justify-between md:block" >
 				<a href="http://www.lunademiel.com.pe/" target="_self">
-					<img src="<?=get_stylesheet_directory_uri().'/build/svg/logo.svg'?>" alt="Luna de Miel agenda" class=" w-24  sm:w-[150px]">
+					<img src="<?=get_stylesheet_directory_uri().'/build/svg/logo.svg'?>" alt="Luna de Miel agenda" class=" w-28  0 sm:w-[150px]">
 				</a>
-				<div  class="inline-flex   md:hidden uppercase space-x-3  " >
+				<div  class="inline-flex  md:hidden uppercase space-x-3  mt-5 " >
 					<span class="text-[#eb3d82] font-bold" >ESPECIAL  </span> <span class="font-bold"  ><?php echo get_the_date( 'F' ); ?> ></span>
 				</div>
 			</div>
 
-			<div class="text-right pt-6 sm:pt-10">
-				<div  class=" hidden-xs md:inline-flex    uppercase space-x-3 " >
+			<div class="text-right pt-0 xs:pt-2 sm:pt-10">
+				<div  class=" agenda-date  uppercase space-x-3 " >
 					<span class="text-[#eb3d82] font-bold" >ESPECIAL  </span> <span class="font-bold"  ><?php echo get_the_date( 'F' ); ?> ></span>
 				</div>
-				<div class=" mt-6 sm:mt-12 text-[35px]  sm:text-[50px] lg:text-[88.5px] tracking-wide " style=" font-family:Didot ;"  >
+				<div class=" mt-6 text-gray-900 sm:mt-12 text-[35px]  sm:text-[50px] lg:text-[88.5px] tracking-wide " style=" font-family:Didot ;"  >
 				#AgendaRomántica
 				</div> 
-				<p  class=" text-[15px]  sm:text-[20px] lg:text-[22.53px]  " >Ideas para romper la rutina y disfrutar toda la semana</p>
+				<p  class=" text-[12px]   sm:text-[20px] lg:text-[22.53px]  " >Ideas para romper la rutina y disfrutar toda la semana</p>
 			</div>
 		</div>
 
-    <div class="separador marginVertical_small">&nbsp;</div>
-    <div class="text-right">
+    <div class="agenda-line mt-5 mb-6">&nbsp;</div>
+    <div class="text-right px-4 lg:px-0">
       <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_page_link();?>"   target="_blank" class="text-primary-500 text-[15px]  md:text-[19px] font-semibold inline-flex items-center">
         ¡Compártelo!&nbsp;&nbsp;<img class="w-[30px] md:w-[50px] h-[30px] md:h-[50px] " src="<?=get_stylesheet_directory_uri().'/build/svg/facebook.svg'?>" alt="facebook">
       </a>
     </div>
  
-	<span class="text-center block  text-[16px] md:text-[20px] font-semibold py-8">-
+	<span class="text-center block  text-[16px] md:text-[20px] font-semibold py-8 ">-
 	<?php
 	$number = 1;
 	$termscategory = get_the_terms( $post->ID , 'agenda' );  
@@ -263,7 +89,7 @@ $number_single = 1;
 		}?>
 	 
 	 -</span>
-  	<div class="agenda-single" style="width:100%;margin-bottom:4em;" >
+  	<div class="agenda-single w-full max-w-[873px] m-auto px-4 lg:px-0 mb-4 "  >
 		<?php
 				if (have_posts()) :
 					while (have_posts()):
@@ -276,10 +102,11 @@ $number_single = 1;
 				wp_reset_postdata();
 		?> 
 	</div>  	
-	<div class="separador ">&nbsp;</div>
-		<h2  class="agenda-carousel__title" >Ver más publicaciones</h2> 
+	<div class="agenda-line ">&nbsp;</div>
+	<div  class="px-10 lg:px-0" >
+		<h2  class="agenda-carousel__title inline-flex " >Ver más publicaciones</h2> 
 		<div class="post owl-carousel agenda-carousel-wrap owl-theme">
-					<?php
+						<?php
 						global $post;
 						
 						//query subpages
@@ -317,7 +144,7 @@ $number_single = 1;
 								} 
 								?>
 								 <div class="post-card item"> 
-									<a href="<? the_permalink(); ?>"  >
+									<a href="<?=the_permalink(); ?>"  >
 										<img src="<?php echo $imagethumb; ?>" alt="<?php the_title();?>" class="thumbnail"/> 
 										<p>- <?php if($terms_slugs_string !== "" ) { echo $terms_slugs_string;} else { echo "actividades";} ?> -</p>   
 										<h2><?php the_title();?></h2>
@@ -331,23 +158,34 @@ $number_single = 1;
 						wp_reset_postdata();
 					?>  
 		</div>		  
+	</div>
   </section>
- <footer class="bg-primary-500 py-4 ">
-    <div class="hidden">
-      <a href="http://www.lunademiel.com.pe" class="w_40_desktop w_80 marginVertical_normal">
-        <img src="http://www.lunademiel.com.pe/emailing/2019/11-noviembre/agenda/images/footer_img.svg" alt="Imagen" class="w_100">
-      </a>
-      <div class="w_60_desktop w_80 section_middle_right marginVertical_normal">
-      <div class="w_90_desktop w_80 section_middle_right marginVertical_normal">
-        <a href="https://www.facebook.com/portallunademiel/"  class="w_20_desktop w_20 marginVertical_normal" target="_self"><img src="http://www.lunademiel.com.pe/emailing/2019/11-noviembre/agenda/images/f.svg" alt="Facebook" class="img_normal img_small_mobile"></a>
-        <div class="w_70 w_65_desktop whiteColor " style="margin-left:15px;">Únete a nuestra comunidad</div>
+ <footer class="bg-primary-500 py-20 px-6 lg:px-0  mt-20">
+    <div class="  md:flex  justify-between text-white w-full max-w-[873px] m-auto  space-x-6 ">
+      <div  class="  inline-flex text-[18px]   sm:text-[20px]  lg:text-[24px]  items-center space-x-10 font-prelo tracking-wider " >
+		<a href="http://www.lunademiel.com.pe" class="block">
+			<img src="<?=get_stylesheet_directory_uri().'/build/svg/hand.svg'?>" alt="Imagen" class=" w-[45px]   md:w-[60px] lg:w-[77.7px]">
+		</a>
+		<span>
+		Artículos, actividades y <br>
+		tips para ti y tu pareja.
+		</span>
+	  </div>
+      <div class="flex items-center text-[14px]  md:text-[16.8px] font-semibold font-prelo tracking-wider mt-10 md:mt-0 ">
+		<div>
+		<div class="flex items-center space-x-8  mb-6">
+			<a href="https://www.facebook.com/portallunademiel/"   target="_self">
+			<img src="<?=get_stylesheet_directory_uri().'/build/svg/facebook-white.svg'?>" alt="Facebook" class=" min-w-[35px]   sm:w-[48px] "></a>
+			<div>Únete a nuestra comunidad</div>
 
-      </div>
-       <div class="w_90_desktop  w_80 section_middle_right  marginVertical_normal" >
-        <a href="http://www.lunademiel.com.pe/contacto/"  class="w_20_desktop w_20 marginVertical_normal" target="_self"><img src="http://www.lunademiel.com.pe/emailing/2019/11-noviembre/agenda/images/correo.svg" alt="Correo" class="img_normal img_small_mobile"></a>
-        <div class="w_70 w_65_desktop whiteColor "  style="margin-left:15px;">si tienes algún producto o servicio para parejas. ¡escribenos!</div>
-      </div>
-    </div>
+		</div>
+		<div class="flex items-center space-x-8  mt-6" >
+			<a href="http://www.lunademiel.com.pe/contacto/" class="block"  target="_self">
+				<img src="<?=get_stylesheet_directory_uri().'/build/svg/email.svg'?>" alt="Correo" class=" min-w-[35px]   sm:w-[48px]  "></a>
+			<div>Si tienes algún producto o servicio para <br class="hidden-xs" > parejas. ¡escribenos!</div>
+		</div>
+		</div>
+		</div>
     </div>
   </footer>
   </div>
