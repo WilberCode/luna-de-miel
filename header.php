1e0e47f4c14132abd70e67f4031ceba64a9a1ccb
wilber <parionahuaraca@gmail.com>
2021-07-14 18:02:23 -0500

    cambiosa

--- header.php
+++ header.php
@@ -203,7 +203,7 @@ restaurantes románticos, tendencias de boda, celebridades, catering, bares y ca
     googletag.enableServices();
   });
 </script>   
-<script data-ad-client="ca-pub-2072313038095874" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+   <script data-ad-client="ca-pub-2072313038095874" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
 </head>
 
