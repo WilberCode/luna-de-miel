d93558b405ee6b171c0b0db0ee019111d9758527
wilber <parionahuaraca@gmail.com>
2021-07-14 13:48:52 -0500

    cambiosa

--- header.php
+++ header.php
@@ -128,7 +128,7 @@ restaurantes románticos, tendencias de boda, celebridades, catering, bares y ca
 	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" />
 	
 	 <!-- Bootstrap -->
-	 <link crossorigin="anonymous" rel="stylesheet" id="font-Awesome-Css-css" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" type="text/css" media="all">
+	<!--  <link crossorigin="anonymous" rel="stylesheet" id="font-Awesome-Css-css" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" type="text/css" media="all"> -->
 	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/_/css/bootstrap.min.css">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
