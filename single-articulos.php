df35cb4939d5ba19d792b0d4d69b16ba935bee01
wilber <parionahuaraca@gmail.com>
2021-07-20 18:22:39 -0500

    CAMBIOS

--- single-articulos.php
+++ single-articulos.php
@@ -27,7 +27,19 @@
 		<?php //endif; ?>	
 		<div class="entry">
 			<?php the_content(); ?>
-		</div> 	
+		</div> 
+		<div  class="ins-wrap" >
+				<!-- Post - Content Footer -->
+				<ins class="adsbygoogle"
+				style="display:block"
+				data-ad-client="ca-pub-2072313038095874"
+				data-ad-slot="8266333524"
+				data-ad-format="auto"
+				data-full-width-responsive="true"></ins>
+			<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+			</script> 
+		</div>
 	<section class="related">
 		<h3>También te puede interesar:</h3>
 		<div class="row">
