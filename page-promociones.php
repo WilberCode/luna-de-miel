6cd5e94e41b60d2e1ebcbf26132c374bebbb3d71
wilber <parionahuaraca@gmail.com>
2021-07-21 17:39:52 -0500

    cambios

--- page-promociones.php
+++ page-promociones.php
@@ -9,18 +9,18 @@
 	<section class="publicidad   ">
 		<div class="container">
 			<div class="row">
-				<div class="ins-wrap justify-center">			 
-
-  					<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2072313038095874" crossorigin="anonymous"></script>
-					 
-					<!-- Categoria - top -->
-					<ins class="adsbygoogle"
-						style="display:inline-block;width:728px;height:90px"
-						data-ad-client="ca-pub-2072313038095874"
-						data-ad-slot="1746007268"></ins>
-					<script>
-						(adsbygoogle = window.adsbygoogle || []).push({});
-					</script>
+				<div class="ins-wrap justify-center">
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2072313038095874"
+					crossorigin="anonymous"></script>
+				<!-- Categoria - top -->
+				<ins class="adsbygoogle"
+					style="display:inline-block;width:728px;height:90px"
+					data-ad-client="ca-pub-2072313038095874"
+					data-ad-slot="1746007268"></ins>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>		 
+  					 
 				</div>
 			</div>
 		</div>
