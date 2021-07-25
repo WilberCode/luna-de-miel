0a86c6ba9071adec2318b75c5cce72bbb4dfa69b
wilber <parionahuaraca@gmail.com>
2021-07-25 03:31:05 -0500

    cbmiso

--- front-page.php
+++ front-page.php
@@ -101,6 +101,22 @@
 </script>
   </div>
 </div>
+  <?php   if(!is_admin()) { ?>  
+      <div  class="sm-from-hide">
+        <div  class="ins-wrap  justify-center " style=" margin-top:6px; margin-bottom:6px;" >
+           <!-- Home - Antes de Novedades -->
+           <ins class="adsbygoogle"
+                style="display:block;"
+                data-ad-client="ca-pub-2072313038095874"
+                data-ad-slot="3701807524"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+                <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        </div>
+      </div>
+    <?php }?> 
 
     <section  id="destacados"  >
       <div class="container"> 
@@ -278,7 +294,7 @@
         </div>
       </div>
     </section> 
-    <section  class="novedades">
+    <section  class="novedades" style="margin-top:4px;" >
       <div class="container"> 
         <div class="row show-grid">
           <div class="hidden-xs col-md-1">&nbsp;</div>
